604d2fe89745e51ef5bb9e90198ead02ea2683d0
Lucas Santos <lucas.santos@acensjr.com>
2022-04-18 21:49:02 -0300

    fixed timezone and finished post route with axios for appointments

--- src/pages/RegistrationForm/RegistrationForm.js
+++ src/pages/RegistrationForm/RegistrationForm.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import ptBR from 'date-fns/locale/pt-BR';
+import axios from '../../services/api'
 import DatePicker from '../../components/datepicker/DatePicker'
 import ChakraInput from '../../components/Input/Input'
 import { Button } from '@chakra-ui/react'
+import moment from 'moment';
 
 function RegistrationForm () {
   const initialValues = {
@@ -14,27 +16,17 @@ function RegistrationForm () {
   }
 
   const validationSchema = Yup.object({
-            name: Yup.string()
-            .min(3)
-            .max(70)
-            .required(),
-
-            birthdate: Yup
-            .date()
-            /* .max('now')
-            .iso() */
-            .required(),
-
-            appointmentDate: Yup
-            .date()
-            /* .min('now')
-            .iso() */
-            .required()
+            name: Yup.string().min(3).max(70).required(),
+            birthdate: Yup.date().required(),
+            appointmentDate: Yup.date().required()
   })
 
-  const onSubmit = values => {
-    console.log('Form data', values)
-    console.log('saved data', JSON.parse(JSON.stringify(values)))
+  const onSubmit = async (values) => {
+    const data = JSON.parse(JSON.stringify(values))
+    const newDate = moment(data.appointmentDate, moment.ISO_8601).local().utc(-3, true).format()
+    data.appointmentDate = newDate
+    console.log(data)
+    await axios.post("/create_appointment", data);
   }
 
   return (
@@ -70,6 +62,7 @@ function RegistrationForm () {
             timeIntervals={60}
             timeCaption="time"
             dateFormat="MMMM d, yyyy h:mm aa"
+            locale={ptBR}
             minDate={new Date(new Date().getTime() + 86400000)}
             />
             <Button type='submit' colorScheme='teal' variant='outline'  disabled={!formik.isValid}>Submit</Button>
